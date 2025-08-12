acffdd68fb7ba1f52b8729e1e34f5796d14f22d4
ozaqua <34608946+ozaqua@users.noreply.github.com>
2025-08-12 18:41:55 +1000

    Update page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -11,7 +11,7 @@ export default function Page() {
     const sql = neon(`${process.env.DATABASE_URL}`);
     const comment = formData.get('comment');
     // Insert the comment from the form into the Postgres database
-    await sql(insert into comments (comment) values ($1), [comment])
+    await sql(insert ',' into comments (comment) values ($1), [comment])
   }
 
   return (
